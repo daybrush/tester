17ad4e9d8f3168f95a4dce646e98c2ed2297250b
Daybrush <daybrush@gmail.com>
2019-08-28 23:31:00 +0900

    fix: fix config

--- karma.config.js
+++ karma.config.js
@@ -1,50 +1,65 @@
-module.exports = function (config) {
-    const karmaConfig = {
-        frameworks: ["mocha", "chai", "karma-typescript"],
-        mime: {
-            'text/x-typescript': ['ts', 'tsx']
+const karmaConfig = {
+    frameworks: ["mocha", "chai", "karma-typescript"],
+    mime: {
+        'text/x-typescript': ['ts', 'tsx']
+    },
+    client: {
+        mocha: {
+            opts: "./mocha.opts",
         },
-        client: {
-            mocha: {
-                opts: "./mocha.opts",
+    },
+    files: [
+        "src/**/*.ts",
+        "src/**/*.tsx",
+        "./test/**/*.ts",
+    ],
+    preprocessors: {
+        "src/**/*.ts": ["karma-typescript"],
+        "src/**/*.tsx": ["karma-typescript"],
+        "test/**/*.ts": ["karma-typescript"],
+    },
+    karmaTypescriptConfig: {
+        tsconfig: "./tsconfig.test.json",
+        reports: {
+            html: {
+                "directory": "coverage",
+                "subdirectory": "./"
+            },
+            lcovonly: {
+                "directory": "coverage",
+                "filename": "lcov.info",
+                "subdirectory": "."
             },
         },
-        files: [
-            "./src/**/*.ts",
-            "./test/**/*.ts",
-        ],
-        preprocessors: {
-            "src/**/*.ts": ["karma-typescript"],
-            "test/**/*.ts": ["karma-typescript"],
+        coverageOptions: {
+            instrumentation: true,
+            exclude: /test/i,
         },
-        karmaTypescriptConfig: {
-            tsconfig: "./tsconfig.test.json",
-            reports: {
-                html: {
-                    "directory": "coverage",
-                    "subdirectory": "./"
-                },
-                lcovonly: {
-                    "directory": "coverage",
-                    "filename": "lcov.info",
-                    "subdirectory": "."
-                },
-            },
-            coverageOptions: {
-                instrumentation: true,
-                exclude: /test/i,
-            },
+    },
+    browsers: [],
+    customLaunchers: {
+        CustomChromeHeadless: {
+            base: "ChromeHeadless",
+            flags: ["--window-size=400,300", "--no-sandbox", "--disable-setuid-sandbox"],
         },
-        browsers: [],
-        customLaunchers: {
-            CustomChromeHeadless: {
-                base: "ChromeHeadless",
-                flags: ["--window-size=400,300", "--no-sandbox", "--disable-setuid-sandbox"],
-            },
-        },
-        reporters: ["mocha"],
-    };
+    },
+    reporters: ["mocha"],
+};
 
+
+exports.setFiles = function (list) {
+    karmaConfig.files = list;
+    const preprocessors = {};
+
+    list.forEach(file => {
+        preprocessors[file] = ["karma-typescript"];
+    })
+    karmaConfig.preprocessors = preprocessors;
+}
+exports.setConfig = function (callback) {
+    callback(karmaConfig);
+}
+exports.karmaConfig = function (config) {
     karmaConfig.browsers.push(config.chrome ? "Chrome" : "CustomChromeHeadless");
 
     if (config.coverage) {
